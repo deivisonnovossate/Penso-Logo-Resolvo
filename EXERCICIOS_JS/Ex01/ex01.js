7378213464ecfef2f8728715d2c9c7db5e34c800
Deivison Novossate <deivisonnra@gmail.com>
2023-03-30 02:08:45 -0300

    Adicionando primeiro exercício do Curso

---
+++ EXERCICIOS_JS/Ex01/ex01.js
@@ -0,0 +1,16 @@
+// Criação das variáveis´
+
+// Variavel String
+
+let nome = 'Deivison Novossate';
+console.log(nome);
+
+// Variavel Number
+
+let idade = 33;
+console.log(idade);
+
+// Variavel Boolean
+
+let homem = true;
+console.log(homem)
