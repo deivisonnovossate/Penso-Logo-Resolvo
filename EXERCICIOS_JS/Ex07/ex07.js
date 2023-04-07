8bbeee5c7e78b2dac8fec6fb5a3fc222da1279ae
Deivison Novossate <deivisonnra@gmail.com>
2023-04-07 14:52:38 -0300

    Adicionando Novos Exercicios

--- EXERCICIOS_JS/Ex07/ex07.js
+++ EXERCICIOS_JS/Ex07/ex07.js
@@ -1,10 +1,43 @@
-
-    let data = document.getElementById('data').value;
-    console.log(data)
-
-    let mes = data.getMonth();
+function obterMes() {
+    let dataMes = document.getElementById('Date').value;
     
-
-    function dataMes(){
-        console.log(mes);
+    switch (new Date(dataMes).getMonth()){
+        case 0:
+            month = 'Janeiro'
+        break;
+        case 1:
+            month = 'Fevereiro';
+        break;
+        case 2:
+            month = 'Março';
+        break;
+        case 3:
+            month = 'Abril';
+        break;
+        case 4:
+            month = 'Maio';
+        break;
+        case 5:
+            month = 'Junho';
+        break;
+        case 6:
+            month = 'Julho';
+        break;
+        case 7:
+            month = 'Agosto';
+        break;
+        case 8:
+            month = 'Setembro';
+        break;
+        case 9:
+            month = 'Outubro';
+        break;
+        case 10:
+            month = 'Novembro';
+        break;
+        case 11:
+            month = 'Dezembro';
     }
+
+    document.getElementById('escrevaData').innerHTML = 'O mês da data selecionada é: ' + month
+}
