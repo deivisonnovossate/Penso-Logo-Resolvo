6d142592044d675d184825eebf296ae5e8dbbeac
Deivison Novossate <deivisonnra@gmail.com>
2023-04-07 21:10:01 -0300

    Delete Ex09 directory

--- Ex09/ex09.js
+++
@@ -1,7 +0,0 @@
-let frase = document.getElementById('fraseParaReverso');
-
-function fraseReversa(){
-  let fraseReverter =  frase.value;
-  let separarCaracter =  fraseReverter.split("").reverse().join("");
-  document.getElementById('mostrarReverso').innerHTML = separarCaracter;
-}
